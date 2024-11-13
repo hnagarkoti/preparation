6468013f25e427d7b8c8ce9a6c518e1e98a574d6
dnegi81 <devendrnegi@dckap.com>
2024-11-13 15:06:30 +0530

    add 2 numbers linked list

--- basic/AddTwoNumbers.js
+++ basic/AddTwoNumbers.js
@@ -23,3 +23,59 @@
 // Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
 // Output: [8,9,9,9,0,0,0,1]
 
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    let output = new ListNode();
+    let current = output;
+    let carry = 0;
+
+    while(l1 || l2) {
+        let num = 0;
+        if(l1 && l2) {
+            num = l1.val + l2.val + carry;
+        }
+        else if(!l1) {
+            num = l2.val + carry;
+        } else if(!l2) {
+            num = l1.val + carry;
+        }
+
+        if(num <= 9){
+            carry = 0;
+            num = num;
+        } else {
+            carry = 1;
+            num = num - 10
+        }
+        current.val = num;
+        if(l1) {
+            l1 = l1.next ? l1.next : null;
+        }
+        if(l2) {
+            l2 = l2.next ? l2.next : null;
+        }
+        if(!l1 && !l2 && carry == 1){
+            current.next = new ListNode();
+            current = current.next;
+            current.val = carry;
+        } else if(!l1 && !l2 && carry == 0) {
+            break;
+        } else{
+            current.next = new ListNode()
+            current = current.next;
+        }
+    }
+
+    return output;
+};
